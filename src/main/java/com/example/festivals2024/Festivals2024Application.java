0edbaf30792169a62478932dbdc8d38efdb5b36c
Alexander Bal <alexander.bal@student.hogent.be>
2024-06-14 17:30:48 +0200

    initial commit

---
+++ src/main/java/com/example/festivals2024/Festivals2024Application.java
@@ -0,0 +1,13 @@
+package com.example.festivals2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Festivals2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Festivals2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/festivals2024/Festivals2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.festivals2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Festivals2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
